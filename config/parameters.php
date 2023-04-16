283ae7eb3aa55ba4debe5e2aaddda9bb0119e6fb
aguilleng0475 <aguilleng0475@uvirtual.ac.cr>
2023-04-16 10:42:30 -0600

    rollback for base_url parameter

--- config/parameters.php
+++ config/parameters.php
@@ -1,4 +1,4 @@
 <?php
-define("base_url", "http://localhost/tienda_principal/");
+define("base_url", "https://localhost/new_projects/NewTienda/");
 define("base_url2", "https://localhost/new_projects/NewTienda/");
 define("base_url3", "https://tiendaadmin.tecnosula.com/");
